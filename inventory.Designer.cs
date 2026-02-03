210f346a51f5b2b212061ab197c083c352f9b7ea
Naldo Vari <72579666+TheGodOfNoodles@users.noreply.github.com>
2026-02-03 12:40:18 +0000

    Improve UI styling for forms

--- inventory.Designer.cs
+++ inventory.Designer.cs
@@ -29,11 +29,30 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.lblInventoryTitle = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblInventoryTitle
+            // 
+            this.lblInventoryTitle.AutoSize = true;
+            this.lblInventoryTitle.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Bold);
+            this.lblInventoryTitle.Location = new System.Drawing.Point(24, 22);
+            this.lblInventoryTitle.Name = "lblInventoryTitle";
+            this.lblInventoryTitle.Size = new System.Drawing.Size(137, 37);
+            this.lblInventoryTitle.TabIndex = 0;
+            this.lblInventoryTitle.Text = "Inventory";
+            // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Honeydew;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblInventoryTitle);
             this.Text = "inventory";
+            this.ResumeLayout(false);
+            this.PerformLayout();
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblInventoryTitle;
     }
 }
--- shop.Designer.cs
+++ shop.Designer.cs
@@ -30,13 +30,14 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.inventoryOpen = new System.Windows.Forms.Button();
+            this.lblTagline = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Imprint MT Shadow", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(281, 19);
+            this.label1.Location = new System.Drawing.Point(281, 28);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(226, 75);
             this.label1.TabIndex = 0;
@@ -44,14 +45,26 @@
             // 
             // inventoryOpen
             // 
-            this.inventoryOpen.Font = new System.Drawing.Font("Imprint MT Shadow", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.inventoryOpen.Location = new System.Drawing.Point(519, 150);
+            this.inventoryOpen.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.inventoryOpen.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.inventoryOpen.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            this.inventoryOpen.Location = new System.Drawing.Point(519, 180);
             this.inventoryOpen.Name = "inventoryOpen";
-            this.inventoryOpen.Size = new System.Drawing.Size(171, 64);
+            this.inventoryOpen.Size = new System.Drawing.Size(171, 52);
             this.inventoryOpen.TabIndex = 1;
             this.inventoryOpen.Text = "Inventory";
-            this.inventoryOpen.UseVisualStyleBackColor = true;
+            this.inventoryOpen.UseVisualStyleBackColor = false;
             this.inventoryOpen.Click += new System.EventHandler(this.inventoryOpen_Click);
+            // 
+            // lblTagline
+            // 
+            this.lblTagline.AutoSize = true;
+            this.lblTagline.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic);
+            this.lblTagline.Location = new System.Drawing.Point(282, 103);
+            this.lblTagline.Name = "lblTagline";
+            this.lblTagline.Size = new System.Drawing.Size(226, 21);
+            this.lblTagline.TabIndex = 2;
+            this.lblTagline.Text = "Spend your rewards wisely!";
             // 
             // shop
             // 
@@ -59,6 +72,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
             this.ClientSize = new System.Drawing.Size(800, 564);
+            this.Controls.Add(this.lblTagline);
             this.Controls.Add(this.inventoryOpen);
             this.Controls.Add(this.label1);
             this.Name = "shop";
@@ -72,5 +86,6 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button inventoryOpen;
+        private System.Windows.Forms.Label lblTagline;
     }
 }
